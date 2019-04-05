9b0c3344e64dc06ad6c5ded8e1f63730ada45aea
quanldz <quanldz@nec.vn>
2019-04-05 16:11:45 +0700

    - Add activity AddNode to add more item to RecyclerView - AddNote do not insert data to DataBase, it only return data back to MainActivity. Then, MainActivity will insert data to DataBase

--- app/src/main/java/com/example/demoroomviewmodellivedata/ui/MainActivity.java
+++ app/src/main/java/com/example/demoroomviewmodellivedata/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.demoroomviewmodellivedata.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.demoroomviewmodellivedata.R;
@@ -9,6 +12,7 @@ import com.example.demoroomviewmodellivedata.room.Note;
 import com.example.demoroomviewmodellivedata.ui.adapter.NoteAdapter;
 import com.example.demoroomviewmodellivedata.utils.AppLogger;
 import com.example.demoroomviewmodellivedata.viewmodel.NoteViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -25,10 +29,24 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private NoteAdapter adapter;
 
+    public static final int ADD_NOTE_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddNote = findViewById(R.id.button_add_note);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Khi click FloatingButton => chuyển sang màn hình AddNote để thêm Note
+                // Sau khi thêm Note và save => quay về MainActivity rồi mới thêm dữ liệu vào DataBase
+                // Check data trả về từ AddNote ở callback "onActivityResult"
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+            }
+        });
 
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -54,5 +72,26 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            // Xử lý sau khi dữ liệu trả về từ bên AddNote
+            // Sau khi AddNote nó sẽ insert cái data vào dataBase
+            // Do LiveData => sau khi insert dataBase xong => nó sẽ tự động trả callback "onChanged" cho thằng ViewModel
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title, description, priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
